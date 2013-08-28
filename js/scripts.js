0ca133ea8a40976d2573f3272cea2fdfd700dd24
Ninjaparade <yazjallad@gmail.com>
2013-08-28 12:04:58 -0700

    Week 2 finished
    Added Masonery
    Added Slides JS
    Styled the shizz out of the html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function($) {
 
+	//to make our grid pretty
+	$('.posts').masonry({
+	  // columnWidth: 200,
+	  itemSelector: '.post'
+	});
+
+	//for the gallery
+	$("#slides").slidesjs({
+		navigation : {
+			effect : "fade"
+		}
+
+	});
 });
 
 
